2540525c440a8906c68db55c01a5db4691be2cae
Iris Markofsky <devils_darkness1313@hotmail.com>
2012-03-12 21:21:22 -0700

    exercise 5.2 tests done.

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -29,16 +29,22 @@ describe "LayoutLinks" do
   
   it "should have the right links on the layout" do
     visit root_path
+	
     click_link "About"
     response.should have_selector('title', :content => "About")
+	
     click_link "Help"
-    response.should # fill in
+    response.should have_selector('title', :content => "Help")# fill in
+	
     click_link "Contact"
-    response.should # fill in
+    response.should have_selector('title', :content => "Contact")# fill in
+	
     click_link "Home"
-    response.should # fill in
+    response.should have_selector('title', :content => "Home")# fill in
+	
     click_link "Sign up now!"
-    response.should # fill in
+    response.should have_selector('title', :content => "Sign up")# fill in
+	
 	end
   
 end
