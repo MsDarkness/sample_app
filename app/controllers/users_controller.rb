be298cc6fb9f804790cf5760815a26aae87f72f3
Iris Markofsky <MsDarkness1313@gmail.com>
2012-04-18 18:51:33 -0700

    push to check on another computer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,59 @@
 class UsersController < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
-    @title = @user.name
-  end
+before_filter :authenticate, :only => [:edit, :update]
 
-  def new
-    @user = User.new
-    @title = "Sign up"
-  end 
+
+	def index
+    @title = "All users"
+    @users = User.all
+	end
+	
+	def show
+		@user = User.find(params[:id])
+		@title = @user.name
+	end
+
+	def new
+		@user = User.new
+		@title = "Sign up"
+	end 
   
-def create
-    @user = User.new(params[:user])
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      @title = "Sign up"
-      render 'new'
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+		  sign_in @user
+		  flash[:success] = "Welcome to the Sample App!"
+		  redirect_to @user
+		else
+		  @title = "Sign up"
+		  render 'new'
+		end
+	end
+
+	def edit
+		@title = "Edit user"
+	end 
+	
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(params[:user])
+		  flash[:success] = "Profile updated."
+		  redirect_to @user
+		else
+		  @title = "Edit user"
+		  render 'edit'
+		end
+	end
+
+	private
+
+    def authenticate
+      deny_access unless signed_in?
     end
-  end
+	
+	 def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+	
 end
