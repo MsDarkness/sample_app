6301329b3c2d149f1dbf367af746e7e6f88f3369
Iris Markofsky <MsDarkness1313@gmail.com>
2012-04-19 11:39:04 -0700

    removed publicprofile from attr_accessible user.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
 
 class User < ActiveRecord::Base
   attr_accessor :password
-  attr_accessible :name, :email, :password, :password_confirmation, :publicprofile
+  attr_accessible :name, :email, :password, :password_confirmation
 
   
   has_many :microposts, :dependent => :destroy
